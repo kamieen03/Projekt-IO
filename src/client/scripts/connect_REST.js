106e459a6e9f612be50cf083d4fa133a1f5acfc4
bonczol <filip.bonczyk@student.put.poznan.pl>
2018-11-11 19:37:20 +0100

    -Pole tekstowe -Można zaznaczać tekst i nakładać transformacje

---
+++ src/client/scripts/connect_REST.js
@@ -0,0 +1,10 @@
+function POST(param){
+    var sel = $("#textarea").getSelection().text;
+    fetch('http://localhost:8080/' + sel + "?transforms=" + param)
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(text) {
+            $("#textarea").replaceSelectedText(text);
+        });
+}
